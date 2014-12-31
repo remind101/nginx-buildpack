5d6638da3b40cdb1f9b60c940fc431e0570648cf
Jerry Su <jerry@remind101.com>
2014-12-30 16:07:44 -0800

    Compile with accept-language-module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -6,9 +6,11 @@
 
 NGINX_VERSION=1.5.2
 PCRE_VERSION=8.21
+NGINX_ACCEPT_LANG_VERSION=2f69842
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
+nginx_accept_lang_tarball_url=https://github.com/giom/nginx_accept_language_module/tarball/${NGINX_ACCEPT_LANG_VERSION}
 
 echo "Downloading $nginx_tarball_url"
 curl $nginx_tarball_url | tar xzf -
@@ -18,7 +20,16 @@ cd nginx-${NGINX_VERSION}
 echo "Downloading $pcre_tarball_url"
 curl $pcre_tarball_url | tar xjf -
 
-./configure --with-http_realip_module --with-http_ssl_module --with-http_gzip_static_module --with-pcre=pcre-${PCRE_VERSION} --prefix=$HOME && make install
+echo "Downloading $nginx_accept_lang_tarball_url"
+curl -L $nginx_accept_lang_tarball_url | tar xzf -
+
+./configure \
+  --with-http_realip_module \
+  --with-http_ssl_module \
+  --with-http_gzip_static_module \
+  --with-pcre=pcre-${PCRE_VERSION} \
+  --add-module=giom-nginx_accept_language_module-${NGINX_ACCEPT_LANG_VERSION} \
+  --prefix=$HOME && make install
 
 cd $HOME/sbin
 
